20e56c4ab542a4bf08fbefdb4cf3fe1090cdc282
LeenaMondal08 <leenamondal@gmail.com>
2024-08-17 21:45:41 +0530

    1st commit

---
+++ playingchar.c
@@ -0,0 +1,45 @@
+/*Task
+
+You have to print the character ,ch , in the first line. Then print  in next line. In the last line print the sentence,sen .
+
+Input Format
+
+First, take a character, ch as input.
+Then take the string, s as input.
+Lastly, take the sentence sen as input.
+
+Constraints
+
+Strings for  and  will have fewer than 100 characters, including the newline.
+
+Output Format
+
+Print three lines of output. The first line prints the character,ch .
+The second line prints the string,s .
+The third line prints the sentence,sen .
+
+Sample Input 0
+
+C
+Language
+Welcome To C!!
+Sample Output 0
+
+C
+Language
+Welcome To C!!*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    char ch,s[100],sen[100];
+    scanf("%c\n%s\n%[^\n]%*c",&ch,&s,&sen); 
+    printf("%c\n%s\n%s",ch,s,sen);
+    return 0;
+}
